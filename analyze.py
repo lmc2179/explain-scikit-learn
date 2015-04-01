68fb9d7e1af3e848eab202dfdb3bf9842dd927aa
Louis Cialdella <lmc2179@github.com>
2015-04-01 08:03:11 -0400

    added first version of shapley approximation logic

--- analyze.py
+++ analyze.py
@@ -2,7 +2,7 @@ import numpy as np
 import copy
 import random
 
-class ClassificationExplanationModel(object):
+class ExplainableClassifier(object):
     def __init__(self, training_inputs, training_outputs, feature_names, model):
         """
         Train a model and store it so explanations can be produced later.
@@ -21,18 +21,28 @@ class ClassificationExplanationModel(object):
         return self.model.classes_
 
     def explain_classification(self, x, number_of_samples=1000):
-        feature_contributions = {} #Each value is a vector of class contributions
-        permutations = copy.deepcopy(self.feature_names)
+        feature_contributions = {f:np.zeros(len(self.class_names)) for f in self.feature_names}
+        feature_permutations = copy.deepcopy(self.feature_names)
         for i in range(number_of_samples):
-            # Produce random elements (permutations and input sample), calculate perturbed probability, adjust
-            permutations = random.shuffle(permutations)
+            # Produce random elements (feature_permutations and input sample), calculate perturbed probability, adjust
+            feature_permutations = random.shuffle(feature_permutations)
+            input_sample = self._sample_input_space()
             for feature in self.feature_names:
                 # Get next iteration of feature contribution vector
-                pass
+                substitute_features = feature_permutations[:feature_permutations.index(feature)]
+                #TODO: Input vectors need better names
+                perturbed_input_with_feature = self._tau(x, input_sample, substitute_features+[feature])
+                perturbed_input_without_feature = self._tau(x, input_sample, substitute_features)
+                v1 = self.model.predict_proba(perturbed_input_with_feature)
+                v2 = self.model.predict_proba(perturbed_input_without_feature)
+                feature_permutations[feature] += (v1 - v2)
         # Normalize each contribution vector by number of samples
         for feature, contributions in feature_contributions.values():
             feature_contributions[feature] = contributions * (1.0 / number_of_samples)
         return feature_contributions
 
-    def _produce_input_sample(self):
+    def _sample_input_space(self):
+        return 0.0 #TODO: Basic input sampling scheme - uniform
+
+    def _tau(self, x, y, features): #TODO: Rename: Name was chosen to match strumbelj et. al's notation in first version
         pass
--- tests.py
+++ tests.py
@@ -4,11 +4,11 @@ from sklearn.linear_model import LogisticRegression
 
 class AnalyzerTest(unittest.TestCase):
     def test_approximate_contribution(self):
-        feature_names = ['featuer', 'useless_1', 'useless_2']
+        feature_names = ['feature', 'useless_1', 'useless_2']
         training_inputs = [[0,10,10],[10,10,10]]
         training_outputs = [0,1]
         model = LogisticRegression()
-        analyzer = analyze.ClassificationExplanationModel(training_inputs, training_outputs, feature_names, model)
+        analyzer = analyze.ExplainableClassifier(training_inputs, training_outputs, feature_names, model)
         for inp in training_inputs:
             print(inp)
             print(analyzer.explain_classification(inp))
