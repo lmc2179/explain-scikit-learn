0600885b584205e680151d51f285312adb1f0ff7
Louis Cialdella <lmc2179@github.com>
2015-04-01 23:44:20 -0400

    First working version of analysis

--- analyze.py
+++ analyze.py
@@ -15,17 +15,27 @@ class ExplainableClassifier(object):
         self.model = model
         self.feature_names = feature_names
         self.model.fit(training_inputs, training_outputs)
+        self.training_min, self.training_max = self._get_vector_min_max(training_inputs)
+
+    def _get_vector_min_max(self, vectors):
+        min_vector = vectors[0]
+        max_vector = vectors[0]
+        for v in vectors[0:]:
+            min_vector = [min(v1,v2) for v1, v2 in zip(v, min_vector)]
+            max_vector = [max(v1,v2) for v1, v2 in zip(v, max_vector)]
+        return min_vector, max_vector
+
 
     @property
     def class_names(self):
         return self.model.classes_
 
     def explain_classification(self, x, number_of_samples=1000):
-        feature_contributions = {f:np.zeros(len(self.class_names)) for f in self.feature_names}
+        feature_contributions = {f:np.zeros((1,len(self.class_names))) for f in self.feature_names}
         feature_permutations = copy.deepcopy(self.feature_names)
         for i in range(number_of_samples):
             # Produce random elements (feature_permutations and input sample), calculate perturbed probability, adjust
-            feature_permutations = random.shuffle(feature_permutations)
+            random.shuffle(feature_permutations)
             input_sample = self._sample_input_space()
             for feature in self.feature_names:
                 # Get next iteration of feature contribution vector
@@ -35,14 +45,14 @@ class ExplainableClassifier(object):
                 perturbed_input_without_feature = self._tau(x, input_sample, substitute_features)
                 v1 = self.model.predict_proba(perturbed_input_with_feature)
                 v2 = self.model.predict_proba(perturbed_input_without_feature)
-                feature_permutations[feature] += (v1 - v2)
+                feature_contributions[feature] += (v1 - v2)
         # Normalize each contribution vector by number of samples
-        for feature, contributions in feature_contributions.values():
+        for feature, contributions in feature_contributions.items():
             feature_contributions[feature] = contributions * (1.0 / number_of_samples)
         return feature_contributions
 
     def _sample_input_space(self):
-        return 0.0 #TODO: Basic input sampling scheme - uniform
+        return [random.uniform(a,b) for a,b in zip(self.training_min, self.training_max)]
 
-    def _tau(self, x, y, features): #TODO: Rename: Name was chosen to match strumbelj et. al's notation in first version
-        pass
+    def _tau(self, x, y, substituted_features): #TODO: Rename: Name was chosen to match strumbelj et. al's notation in first version
+        return [x[i] if feature in substituted_features else y[i] for i, feature in enumerate(self.feature_names)]
