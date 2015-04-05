efd34cd59a4fe0b71c93d24fa11a6ad5cc7fe0c4
Louis Cialdella <lmc2179@github.com>
2015-04-04 22:31:05 -0400

    Added iris test

--- tests.py
+++ tests.py
@@ -2,8 +2,15 @@ import unittest
 import analyze
 import pprint
 from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+import pandas as pd
+
+#TODO: Separate explanation mechanism tests from visualization tests
+#TODO: Add assertions to explanation mechanism tests
+#TODO: Add test for Titanic data set
 
 class AnalyzerTest(unittest.TestCase):
+    @unittest.skip('Temp')
     def test_approximate_contribution(self):
         feature_names = ['feature', 'useless_1', 'useless_2']
         training_inputs = [[0,10,10],[10,10,10]]
@@ -13,3 +20,11 @@ class AnalyzerTest(unittest.TestCase):
         explanations = [analyzer.explain_classification(inp, 1000) for inp in training_inputs]
         pprint.pprint(list(zip(training_inputs,explanations)))
         analyze.BarPlot(explanations[0])
+
+    def test_iris(self):
+        data = datasets.load_iris()
+        model = LogisticRegression()
+        target = [data.target_names[t] for t in data.target]
+        analyzer = analyze.ExplainableClassifier(data.data, target, data.feature_names, model)
+        explanation = analyzer.explain_classification(data.data[0])
+        analyze.BarPlot(explanation)
